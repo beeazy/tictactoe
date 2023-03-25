1e1b4d0d543e55cd3002a666def8cd8d296b1d1e
EvansMutwiri <evans.mutwiri@apollo.co.ke>
2023-03-25 14:33:15 +0300

    Initial commit

---
+++ src/main/kotlin/com/beeazy/plugins/Monitoring.kt
@@ -0,0 +1,23 @@
+package com.beeazy.plugins
+
+import io.ktor.server.plugins.callloging.*
+import org.slf4j.event.*
+import io.ktor.server.request.*
+import io.ktor.server.metrics.dropwizard.*
+import com.codahale.metrics.*
+import io.ktor.server.application.*
+
+fun Application.configureMonitoring() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+    install(DropwizardMetrics) {
+        Slf4jReporter.forRegistry(registry)
+            .outputTo(this@module.log)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build()
+            .start(10, TimeUnit.SECONDS)
+    }
+}
---
+++ src/test/kotlin/com/beeazy/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.beeazy
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.ktor.http.*
+import com.beeazy.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
